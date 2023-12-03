33747a3387c2079b866fbffa0594720961f449bc
Kasita Sansanthad <124168868+gkasita@users.noreply.github.com>
2023-12-03 09:19:58 +0700

    Delete rule_base.pl

--- rule_base.pl
+++
@@ -1,8 +0,0 @@
-% rule_base.pl
-
-% Define rules for various intents
-rule(inquire_requirements, [user_asks(What), admission_requirements(Requirements)]) :-
-    % ...
-
-rule(check_application_status, [user_asks(What), application_status(Status)]) :-
-    % ...
